5f92c193519c95b01e41e1200228ae03ebf3679b
Kaito-Von-Vocaloid <84059078+Kaito-Von-Vocaloid@users.noreply.github.com>
2021-12-26 16:07:08 +0300

    Create dolarkur2.js

---
+++ dolarkur2.js
@@ -0,0 +1,6 @@
+axios.get('https://www.google.com.tr/search?q=dolar+tl').then(res => {
+	var data = res.data;
+	var splN = data.toString().split('\n');
+	var kur = splN[10].substring(4761, 4766);
+	console.log(kur);
+});
